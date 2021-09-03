6a6b01b691212b1bc23afd3c81f27923bd2d1e5f
designC <znehraks@gmail.com>
2021-09-03 18:35:30 +0900

    setup graphql and prisma

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log(`Server is Running on http://localhost:${PORT}/`));
